955d9f997e194a003020736630a9b73856bd0c53
makkentoshi <pochta.zdraste@mail.ru>
2024-07-30 02:50:54 +0500

    chore: Update UserSchema to include timestamps

--- app/Models/UserSchema.ts
+++ app/Models/UserSchema.ts
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  clerkUserId: { type: String, required: true },
-  emailAddress: { type: String, required: true },
-});
+const UserSchema = new mongoose.Schema(
+  {
+    clerkUserId: { type: String, required: true },
+    emailAddress: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
 
 export default User;
