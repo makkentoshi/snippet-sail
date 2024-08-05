d331b594f3b3b13816ed482500381e068b2459ee
makkentoshi <pochta.zdraste@mail.ru>
2024-08-05 09:07:29 +0500

    Refactor MobileSideBar component and update icons

--- app/my-notes/components/MobileSideBar.tsx
+++ app/my-notes/components/MobileSideBar.tsx
@@ -1,7 +1,21 @@
 "use client";
-import React from "react";
-import { Button } from "antd";
-import { MenuFoldOutlined } from "@ant-design/icons";
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  HomeOutlined,
+  AppstoreOutlined,
+  PayCircleOutlined,
+  SettingOutlined,
+  BarsOutlined,
+  CreditCardOutlined,
+  JavaScriptOutlined,
+  PythonOutlined,
+  Html5Outlined,
+  JavaOutlined,
+  AlignLeftOutlined,
+  MenuFoldOutlined,
+} from "@ant-design/icons";
+import { Menu } from "antd";
 
 const MobileTopbar = ({
   collapsed,
@@ -10,16 +24,48 @@ const MobileTopbar = ({
   collapsed: boolean;
   setCollapsed: (collapsed: boolean) => void;
 }) => {
+  const [current, setCurrent] = useState("mail");
   return (
-    <div className="w-full h-14 bg-gray-100 flex items-center px-4 shadow-md fixed top-0 left-0 z-20">
-      <Button
-        type="text"
-        onClick={() => setCollapsed(!collapsed)}
-        className="mr-4"
-      >
-        <MenuFoldOutlined />
+    <div className="w-full h-14 bg-white flex items-center px-4 shadow-md fixed top-0 left-0 z-20">
+      <Button>
+        <Menu mode="horizontal" onClick={() => {}} selectedKeys={[current]}>
+          <Menu.Item key="home" icon={<HomeOutlined />}>
+            Home
+          </Menu.Item>
+          <Menu.Item key="chat" icon={<AppstoreOutlined />}>
+            Chat
+          </Menu.Item>
+          <Menu.SubMenu
+            key="subsnippets"
+            icon={<BarsOutlined />}
+            title="Snippets"
+          >
+            <Menu.Item key="all" icon={<AlignLeftOutlined />}>
+              <span className="">All Snippets</span>
+            </Menu.Item>
+            <Menu.Item key="pythonsnippets" icon={<PythonOutlined />}>
+              <span className="">Python</span>
+            </Menu.Item>
+            <Menu.Item key="jssnippets" icon={<JavaScriptOutlined />}>
+              <span className="">JavaScript</span>
+            </Menu.Item>
+
+            <Menu.Item key="csssnippets" icon={<Html5Outlined />}>
+              <span className="">HTML & CSS</span>
+            </Menu.Item>
+            <Menu.Item key="java" icon={<JavaOutlined />}>
+              <span className="">Java</span>
+            </Menu.Item>
+          </Menu.SubMenu>
+          <Menu.Item key="donate" icon={<CreditCardOutlined />}>
+            Donate
+          </Menu.Item>
+          <Menu.Item key="settings" icon={<SettingOutlined />}>
+            Settings
+          </Menu.Item>
+        </Menu>
       </Button>
-      <span className="text-lg font-semibold">Menu</span>
+      {/* <span className="text-lg font-semibold">Menu</span> */}
     </div>
   );
 };
